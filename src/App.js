7a325a0310000168e7fff2b762217e0329104a6f
DEV-MUKHID <mukhid976@gmail.com>
2024-07-08 19:05:18 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,53 +1,43 @@
+import React from 'react';
 import logo from './logo.jpeg';
 import './App.css';
-import { Button, Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 
 function App() {
-
-  let lebar = window.innerWidth;
-  
   return (
-    <>
-      <Container>
-        <Row>
-          <Col md={lebar <= 1000 ? 12 : 2} className='d-flex justify-content-center'>
-            <img src={logo} style={lebar <= 1000 ? {width:'19%', rotate:'90deg', marginTop:'-9rem'} : {height:'100vh'}}/>
-          </Col>
-          <Col md={10}>
-            <div style={lebar <= 1000 ? {marginTop:'-10rem'} : null}>
-              <h3 style={{marginTop:15, fontWeight:'bold'}}>Gentapangan Pasuruan raya 1189</h3>
+    <Container>
+      <Row className="vh-100">
+        <Col xs={12} md={3} className="d-flex justify-content-center align-items-center flex-column text-center">
+          <img src={logo} alt="Pasuruan Raya Logo" className="img-fluid" style={{ maxWidth: '300px', objectFit: 'contain' }} />
+          <h2 className="font-weight-bold mt-3">Pasuruan Raya</h2>
+        </Col>
+        <Col xs={12} md={9} className="d-flex justify-content-center align-items-start flex-column">
+          <div>
+            <h3 className="font-weight-bold">GERAKAN NASIONAL TANI UNTUK KEDAULATAN PANGAN</h3>
 
-              <div>
-                <h5 style={{marginTop:15, fontWeight:'bold'}}>Contact Person</h5>
-                <Button variant="primary" size="lg">
-                    CP 1 : 085791567569
-                </Button>
-                <Button variant="warning" size="lg" style={lebar <= 1000 ? {marginTop:5} : {marginLeft:5}}>
-                    CP 2 : 081233960068
-                </Button>
-              </div>
-
-              <div>
-                <h5 style={{marginTop:15, fontWeight:'bold'}}>Alamat</h5>
-
-                <div>
-                  <iframe
-                    width="100%"
-                    height="450rem"
-                    id="gmap_canvas"
-                    src={
-                      'https://maps.google.com/maps?q=-7.729111,112.71775&z=15&output=embed'
-                    }
-                    className="mt-2 maps"
-                  />
-                </div>
-                
-              </div>
+            <div className="d-flex align-items-center mt-3">
+              <div className="font-weight-bold" style={{ minWidth: '100px', fontWeight:'bold' }}>Alamat:</div>
+              <div>Jl. SMPN 01 Dsn. Sengkan RT. 3 / RW. 6 Kel. Sukorejo Kec. Sukorejo Kab. Pasuruan 67161 JATIM</div>
             </div>
-          </Col>
-        </Row>
-      </Container>
-    </>
+
+            <div className="d-flex align-items-center mt-3">
+              <div className="font-weight-bold" style={{ minWidth: '100px', fontWeight:'bold' }}>Telepon:</div>
+              <div>085791567569, 081233960068, 085645727779</div>
+            </div>
+
+            <div className="d-flex align-items-center mt-3">
+              <div className="font-weight-bold" style={{ minWidth: '100px', fontWeight:'bold' }}>Email:</div>
+              <div>gentapanganpasuruanraya@gmail.com</div>
+            </div>
+
+            <div className="d-flex align-items-center mt-3">
+              <div className="font-weight-bold" style={{ minWidth: '100px', fontWeight:'bold' }}>Website:</div>
+              <div>https://genta-pangan-pasuruan-raya1189.vercel.app/</div>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
